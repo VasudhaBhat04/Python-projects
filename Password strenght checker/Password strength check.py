a9041856501f8a80ed0984f17ebac5d2392b4228
BaymaxBytes! 🤖 <vasudhabhatkp@gmail.com>
2025-03-05 12:08:40 +0530

    Password strength check.py

--- Password strenght checker/Password strength check.py
+++ Password strenght checker/Password strength check.py
@@ -1,3 +1,7 @@
+#Password Strength checker using Python 
+#Evaluates password strength by checking for essential security criteria, including length, uppercase and lowercase letters, digits, and special characters. 
+#It provides information on missing elements and continuously prompts the user until a "Very Strong" password is created.
+
 import re
 
 def check_password_strength(password):
