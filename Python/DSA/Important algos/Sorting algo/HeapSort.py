eb38c3cf532678596dc3a58b4c64df8fd48beba8
vasudha <vasudhabhatkp@gmail.com>
2025-11-11 12:47:24 +0530

    Add

--- Python/DSA/Important algos/Sorting algo/HeapSort.py
+++ Python/DSA/Important algos/Sorting algo/HeapSort.py
@@ -1,4 +1,28 @@
 # Convert array into a heap, repeatedly extract the max element, and rebuild the heap.
+"""
+A heap is a special kind of binary tree that satisfies the heap property:
+Max Heap:
+The parent node is always greater than or equal to its children.
+→ So the largest element is always at the root (index 0).Ascending order sort
+Min Heap:
+The parent node is always smaller than or equal to its children.
+→ So the smallest element is always at the root.Descending order sort.
+
+Heap sort typically uses a Max Heap (for ascending order sort):
+
+Build a Max Heap from the array.
+→ So the largest element is at index 0.
+
+Swap the largest element (root) with the last element.
+
+Reduce heap size (ignore the sorted last element).
+
+Heapify the root again to maintain max heap.
+
+Repeat until the entire array is sorted.
+
+"""
+
 def heapify(arr, n, i):
     largest = i     # Initialize largest as root
     l, r = 2*i + 1, 2*i + 2    # left index = 2*i + 1 , right index = 2*i + 2
@@ -33,3 +57,12 @@ if __name__ == "__main__":
 
     for i in range(len(arr)):
         print(arr[i], end=" ")
+
+"""
+Time Complexity:
+
+Best/Worst/Average: O(n log n)
+
+Space: O(1)
+
+"""
